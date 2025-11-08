a2916ce2611f9e483df1f7586eb89d686996fa0f
Patrick <pobanion@live.com>
2025-11-08 02:52:38 -0600

    esp8266 -> esp32

--- main/main.cpp
+++ main/main.cpp
@@ -1,40 +1,41 @@
-//#include "include/WiFi/WiFi.h"
-//#include "include/esp32/Arduino.h"
+#include "include/WiFi/WiFi.h"
+#include <WiFi.h>
+#include "include/esp32/Arduino.h"
 
-// FreeRTOS API needed for vTaskDelay used in app_main
+#include <string.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
 
-const char* ssid = "";
-const char* password = "";
+#include "lwip/err.h"
+#include "lwip/sys.h"
+
+
+const char* ssid = "aaa";
+const char* password = "sss";
 
 void setup() {
- /* Serial.begin(115200);
+  Serial.begin(115200);
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
-  }lear
+  }
+  //clear
   Serial.println("Connected to Wi-Fi");
   Serial.print("IP Address: ");
-  Serial.println(WiFi.localIP());
+  //Serial.println(WiFi.localIP());
   Serial.print("Gateway (Router IP): ");
-  Serial.println(WiFi.gatewayIP()); // Get router's IP address*/
+  //Serial.println(WiFi.gatewayIP()); // Get router's IP address
 }
-
-/*void app_main(void) {
-  // Empty loop
-}*/
 
 // app_main must use C linkage so the C startup code can find it.
 extern "C" void app_main(void) {
-  // Call Arduino-style setup once, then repeatedly call loop()
-  /*setup();
-
-  while (true) {
-    loop();
-    // Give other tasks CPU time
-    vTaskDelay(pdMS_TO_TICKS(10));
-  }*/
+    setup();
 }
